a32bc1deffc0857062a3b328da55f7aa233d978f
Mizner <michaelmizner@gmail.com>
2021-02-22 08:37:42 -0700

    big refactor

--- tools/utils/env.js
+++
@@ -1,5 +0,0 @@
-import config from '../../package.json';
-
-const mode = process.env.NODE_ENV;
-
-export { mode, config };
--- wp-content/themes/look/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * The Template for the sidebar containing the main widget area
- *
- * @package  WordPress
- * @subpackage  Timber
- */
-
-Timber::render( [ 'dist/markup/sidebar.twig' ], $data );
