91e56e3caad2e1ddf0754a443fd3a1261a9e6e6a
Mizner <michaelmizner@gmail.com>
2021-04-05 09:56:57 -0600

    add notice helper

--- wp-content/plugins/custom/classes/utils/class-render.php
+++ wp-content/plugins/custom/classes/utils/class-render.php
@@ -4,6 +4,6 @@ namespace Custom\Utils;
 
 class Render {
 	public static function notice( $message, $classes = 'notice is-dismissible' ) {
-		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $classes ), esc_html( $message ) );
+		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $classes ), wp_kses_post( $message ) );
 	}
 }
