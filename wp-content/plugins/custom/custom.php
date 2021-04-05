259f5c65fb00a1d54f6fc36b5fad25ccc9805596
Mizner <michaelmizner@gmail.com>
2021-04-05 09:58:21 -0600

    update init file

--- wp-content/plugins/custom/custom.php
+++ wp-content/plugins/custom/custom.php
@@ -14,8 +14,8 @@ namespace Custom;
 defined( 'WPINC' ) || die;
 
 // Constants we'll commonly use in the plugin.
-define( 'PATH', plugin_dir_path( __FILE__ ) );
-define( 'URI', plugin_dir_url( __FILE__ ) );
+define( __NAMESPACE__ . '\PATH', plugin_dir_path( __FILE__ ) );
+define( __NAMESPACE__ . '\URI', plugin_dir_url( __FILE__ ) );
 
 // Loads composer packages.
 require_once PATH . 'vendor/autoload.php';
@@ -26,6 +26,15 @@ require_once PATH . 'lib/class-wpcs-autoloader.php';
 // Action Hook for this plugin to load after all other plugins have been loaded.
 add_action( 'plugins_loaded', __NAMESPACE__ . '\run' );
 function run() {
+
 	Admin_Notices\Dependencies::init();
-	Field_Groups\Options_Page::init();
+	WP_Registrations\Nav_Menus::init();
+	// WP_Registrations\CPT_Staff::init();
+	Timber_Config\Setup::init();
+	Timber_Config\Additions::init();
+	ACF_Field_Groups\Options_Page::init();
+	ACF_Format_Values\Format_Types::init();
+	Timber_Add_Context\Everywhere::init();
+	Timber_Add_Context\Singles::init();
+	ACF_Blocks\Hero::init();
 };
