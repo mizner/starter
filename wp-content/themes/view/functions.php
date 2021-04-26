d91577b62ea5dbb2dafdf6f1748ff8a76390889b
Mizner <michaelmizner@gmail.com>
2021-04-26 17:12:12 -0600

    rework timber paths and include

--- wp-content/themes/view/functions.php
+++ wp-content/themes/view/functions.php
@@ -19,11 +19,13 @@ if ( ! class_exists( Timber::class ) ) {
 add_filter(
 	'custom/view/directories',
 	function( $templates ) {
-		$templates [] = trailingslashit( get_stylesheet_directory() ) . 'dist/components';
-		$templates [] = trailingslashit( get_stylesheet_directory() ) . 'dist/svgs';
-		return $templates;
+		return [
+			trailingslashit( get_stylesheet_directory() ) . 'dist/components',
+			trailingslashit( get_stylesheet_directory() ) . 'dist/svgs',
+		];
 	}
 );
 
 require_once get_stylesheet_directory() . '/inc/enqueues.php';
 require_once get_stylesheet_directory() . '/inc/supports.php';
+require_once get_stylesheet_directory() . '/inc/critical-foft-data-uri.php';
