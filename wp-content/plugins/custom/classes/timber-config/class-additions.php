8ea5e31b208a06dcc66ee970a7986e8eed71f668
Mizner <michaelmizner@gmail.com>
2021-04-28 10:37:13 -0600

    minor reorder

--- wp-content/plugins/custom/classes/timber-config/class-additions.php
+++ wp-content/plugins/custom/classes/timber-config/class-additions.php
@@ -19,8 +19,8 @@ class Additions {
 	 */
 	public function add_to_twig( $twig ) {
 		$twig->addFunction( new Timber\Twig_Function( 'add_script', 'wp_enqueue_script' ) );
-		$twig->addFunction( new Timber\Twig_Function( '_log', '_log' ) );
 		$twig->addFunction( new Timber\Twig_Function( 'add_style', 'wp_enqueue_style' ) );
+		$twig->addFunction( new Timber\Twig_Function( '_log', '_log' ) );
 		$twig->addFunction( new Timber\Twig_Function( 'is_front_page', 'is_front_page' ) );
 
 		$twig->addExtension( new Twig\Extension\StringLoaderExtension() );
