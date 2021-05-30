2998ac9e759cc83170eb81cec93a8bb1506de747
Mizner <michaelmizner@gmail.com>
2021-05-29 20:48:24 -0600

    fix namespace use

--- wp-content/themes/view/single.php
+++ wp-content/themes/view/single.php
@@ -9,6 +9,8 @@
  * @since    Timber 0.1
  */
 
+use Timber\Timber;
+
 $context         = Timber::context();
 $timber_post     = Timber::query_post();
 $context['post'] = $timber_post;
@@ -16,10 +18,13 @@ $context['post'] = $timber_post;
 if ( post_password_required( $timber_post->ID ) ) {
 	Timber::render( 'templates/single-password.twig', $context );
 } else {
-	Timber::render( [
-		'templates/single-' . $timber_post->ID . '.twig',
-		'templates/single-' . $timber_post->post_type . '.twig',
-		'templates/single-' . $timber_post->slug . '.twig',
-		'templates/single.twig'
-	], $context );
+	Timber::render(
+		[
+			'templates/single-' . $timber_post->ID . '.twig',
+			'templates/single-' . $timber_post->post_type . '.twig',
+			'templates/single-' . $timber_post->slug . '.twig',
+			'templates/single.twig',
+		],
+		$context
+	);
 }
