7104c4e66f9864f353df69053cbc2ff02aea7c94
Mizner <michaelmizner@gmail.com>
2021-05-30 16:14:56 -0600

    cleanup enqueues file

--- wp-content/themes/view/inc/enqueues.php
+++ wp-content/themes/view/inc/enqueues.php
@@ -5,17 +5,13 @@
 
 add_action( 'wp_enqueue_scripts', 'Theme_Enqueues::front_end' );
 add_action( 'admin_head', 'Theme_Enqueues::dashboard' );
+add_action( 'admin_enqueue_scripts', 'Theme_Enqueues::dashboard' );
 class Theme_Enqueues {
 	/**
 	 * Enqueue Examples:
-		wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/dist/styles/main.css', [], '1.0' );
-		wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/dist/scripts/main.js', [ 'jquery' ], '1.0', true );
-		wp_localize_script( 'main', 'GLOBAL',
-			[
-				'ajaxURL' => admin_url( 'admin-ajax.php' ),
-				'nonce'   => wp_create_nonce( 'security' ),
-			]
-		);
+	 * wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/dist/styles/main.css', [], '1.0' );
+	 * wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/dist/scripts/main.js', [ 'jquery' ], '1.0', true );
+	 * wp_localize_script( 'main', 'GLOBAL', [ 'ajaxURL' => admin_url( 'admin-ajax.php' ), 'nonce'   => wp_create_nonce( 'security' ), ]);
 	*/
 
 	const DIRECTORIES = [
@@ -27,29 +23,23 @@ class Theme_Enqueues {
 	];
 
 	public static function dashboard() {
-		?>
-		<style>
-		<?php include get_stylesheet_directory() . '/dist/components/base-dev.css'; ?>
-		</style>
-		<?php
+		// Enqueue special class prefixed file for better CSS scoped previews
+		wp_enqueue_style( 'block-previews', get_stylesheet_directory_uri() . '/dist/styles/block-previews.css', [], '1.0' );
 	}
 
 	public static function front_end() {
+		// Register all matching assets for enqueues
+		// Note: does not load these assets, it's only for registration
 		self::register_directory_glob_js( self::DIRECTORIES );
 		self::register_directory_glob_css( self::DIRECTORIES );
-		wp_enqueue_script( 'base' );
 
 		if ( 'production' === wp_get_environment_type() ) {
-			self::register_directory_glob_css();
+			// Enqueue limited base styles
 			wp_enqueue_style( 'base' );
 		} else {
 			// Use unique file for easy CSS reloads during development
 			wp_enqueue_style( 'base-dev', get_stylesheet_directory_uri() . '/dist/components/base-dev.css', [], '1.0' );
 		}
-
-		// TODO: Update inline styles
-		// add_editor_style( 'dist/styles/tailwind.css' );
-		// add_editor_style( 'dist/styles/main.css' );
 	}
 
 	public static function register_directory_glob_js( $directories = [] ) {
